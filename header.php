5d850a7b4b58b844ae5dd5d3b15ea831d62d0927
Iago <iagogtr@gmail.com>
2024-11-21 02:20:24 -0300

    Ajustando menu com bootstrap

--- header.php
+++ header.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <title><?=$title?></title>
+        <title>Bolos Deliciosos</title>
         <link rel="stylesheet" href="styles.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
         <header class="capa">
@@ -12,28 +13,23 @@
                 <h1>Bolos Deliciosos</h1>
                 <h2>Os melhores sabores feitos com amor e tradição</h2>
             </div>
-            
         </header>
-        <nav class="navegacao">
-            <ul>
-                <li>
-                    <a href="index.php">Início</a>
-                </li>
-                <li>
-                    <a href="pedidos.php">Pedidos</a>
-                </li>
-                <li>
-                    <a href="nossa_historia.php">Nossa História</a>
-                </li>
-                <li>
-                    <a href="localizacao.php">Localização</a>
-                </li>
-                <li>
-                    <a href="contato.php">Contato</a>
-                </li>
-                <li>
-                    <a href="login.php">Login</a>
-                </li>
-            </ul>
+        <nav class="navegacao navbar navbar-expand-lg">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">    
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Início</a>
+                    </li>   
+                    <li class="nav-item">
+                        <a class="nav-link" href="form.php">Pedidos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sobre.php">Sobre</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
-    <main>
+        <main>
